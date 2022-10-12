79b8a6a34093ca5ef92e0b54c283c3a1bc6323b4
FlickItUpp <84152232+FlickItUpp@users.noreply.github.com>
2022-10-11 19:01:10 -0500

    HTML, css complete and JS variables set

---
+++ index.js
@@ -0,0 +1,15 @@
+const DEFAULT_MODE = 'color';
+const DEFAULT_COLOR = '#000000';
+const DEFAULT_SIZE = 16;
+
+const colorPicker = document.getElementById('color-picker');
+const colorBtn = document.getElementById('colorBtn');
+const rainbowBtn = document.getElementById('rainbowBtn');
+const shaderBtn = document.getElementById('shaderBtn');
+const eraserBtn = document.getElementById('eraserBtn');
+const clearBtn = document.getElementById('clearBtn');
+const sizeValue = document.getElementById('sizeValue');
+const sizeSlider = document.getElementById('sizeSlider');
+const canvasGrid = document.getElementById('canvas-grid');
+
+
